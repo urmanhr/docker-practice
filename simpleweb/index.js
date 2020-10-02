accc634a6c538db8612d1719a8ad6cf75194ae63
Urman Ratneshwar <urmanhr@gmail.com>
2020-10-02 23:56:33 +0530

    first commit

---
+++ simpleweb/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+    host: 'redi-server',
+    port: 6379
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send('Total visits : ' + visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
